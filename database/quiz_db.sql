4fc2c8a66a9f07530366fe9043fea896061d6258
Densu WorkStation <HELLO@PLZNB2200010100.Supporting.corp.bankmandiri.co.id>
2021-06-19 19:30:24 +0700

    update DB

--- database/quiz_db.sql
+++ database/quiz_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 19, 2021 at 02:06 PM
+-- Generation Time: Jun 19, 2021 at 02:28 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 7.4.19
 
